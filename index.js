86ad9f297953206dbbf8dea9055a69317d38e97f
Marlene Hasslinger <hasslingermarlene@gmail.com>
2019-03-04 18:46:48 -0500

    Filter functionality for projects added

--- index.js
+++ index.js
@@ -2,7 +2,27 @@ jQuery(document).ready(function($) {
     $("#toggle-mobile-nav").click(function() {
         $("#page").toggleClass("mobile-nav-opened");
     });
-})
+
+    /* activate jquery isotope */
+  var $container = $('#posts').isotope({
+    itemSelector : '.item',
+    isFitWidth: true
+  });
+
+  $(window).smartresize(function(){
+    $container.isotope({
+      columnWidth: '.col-sm-3'
+    });
+  });
+  
+  $container.isotope({ filter: '*' });
+
+    // filter items on button click
+  $('#filters').on( 'click', 'button', function() {
+    var filterValue = $(this).attr('data-filter');
+    $container.isotope({ filter: filterValue });
+  });
+});
 
 $("#page").click(function(e) {
     var target = e.target;
